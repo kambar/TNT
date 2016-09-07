65f44ad3f4e85ffee32902a42006b24bdcb33177
bifrostSoft <kamil.barbachowski@outlook.com>
2016-09-07 11:18:45 +0200

    init

---
+++ TNT/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace TNT.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
